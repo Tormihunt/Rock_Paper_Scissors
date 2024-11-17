367d9013b7375a8b05770712dd237d45b65a855e
Tormihunt <tormihunt@gmail.com>
2024-11-17 23:27:29 +0200

    Create new files and write pseudocode
    Write pseudocode in index.js for clarity and to
    understand what to do.

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+PSEUDOCODE:
+Output Explain the rules to the player
+Output Begin round: round numbere
+create player score count and computer score count
+while player or computer has less than 5 score:
+    Decide randomly what computer plays
+    Ask user for input (preferrably choice not type-out)
+    If player played scissors then:
+        If computer played scissors:
+            Output a tie
+            new round, no score change
+        If computer played rock:
+            Output lose
+            computer gets +1 to score
+        If computer played paper:
+            Output win
+            player gets +1 to score
+    Repeat likewise for player playing rock or paper
+    output player and computer score
+If player wins: output congrats you won
+If computer wins; output womp-womp you lose
+*/
+
+alert("Hi! You are playing rock-paper-scissors. The rules are simple:")
+alert("If you play rock and the opponent plays scissors you win. However if the opponent plays paper you would lose and in case of a the opponent also playing a rock it would be a tie!")
+alert("It's best of 5 and rock beats scissors, scissors beat paper and paper beats rock. Lets begin!")
+let player_score = 0
+let computer_score = 0
